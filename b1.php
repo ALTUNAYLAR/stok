9c3bea37c76abd82582fbd1033de9038097a0cf9
sukru altunay <32617922+ALTUNAYLAR@users.noreply.github.com>
2017-11-10 22:52:20 +0300

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: sukru altunay <32617922+ALTUNAYLAR@users.noreply.github.com>
    
    # Conflicts:
    #	deneme.php

---
+++ b1.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Şükrü Altunay
+ * Date: 10.11.2017
+ * Time: 22:45
+ */
+
+
+?>
---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <title>Bootstrap Eğitimi</title>
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/custom.css" rel="stylesheet" type="text/css">
+    <link href="css/normalize.css" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+    <script src="js/jquery.js" type="text/javascript" ></script>
+    <script src="js/bootstrap.js" type="text/javascript" ></script>
+</body>
+
+</html>
